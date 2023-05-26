eca8033df85a1dc1928fed4853ca1de46222f8eb
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-26 17:40:40 +0200

    EntityManager and methods persist, find, merge, remove

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,73 @@
-package org.example;public class Employee {
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees") // możesz nazwać tabelę
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private LocalDate birthday;
+    @Column
+    private String email;
+    public Employee() {
+    }
+
+    public Employee(String name, LocalDate birthday, String email) {
+        this.name = name;
+        this.birthday = birthday;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,54 @@
-package org.example;public class Main {
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee = new Employee("Jan", LocalDate.of(1999, Month.MARCH,2), "jan@wp.pl");
+        Employee employee2 = new Employee("Misia", LocalDate.of(1995, 3,2), "misia@wp.pl");
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        EntityManager entityManager = sessionFactory.createEntityManager();
+
+        // INSERT
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee2);
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
+
+        //FIND - jak robie find to nie musze robic begin transaction
+
+        Employee foundEmployee = entityManager.find(Employee.class, 1);
+        System.out.println("Pracownik o id = 1:");
+        System.out.println(foundEmployee.toString());
+
+        //MERGE czyli update
+        foundEmployee.setEmail("buziaczek@gmail.com");
+        foundEmployee.setName("Lala");
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(foundEmployee);
+        entityManager.getTransaction().commit();
+
+        System.out.println(entityManager.find(Employee.class, foundEmployee.getId()));
+
+        //REMOVE
+//        entityManager.getTransaction().begin();
+//        entityManager.remove(foundEmployee);
+//        entityManager.getTransaction().commit();
+//
+        //poniżej uzywamy HQL do uzyskania listy rekordów z tabeli
+
+        List<Employee> employeeList = entityManager.createQuery("from Employee", Employee.class).getResultList();
+        System.out.println(employeeList);
+
+
+        HibernateUtil.shutdown();
     }
 }
