83e95f9fd4d3190703e53c2d7b8bb0ecafd87f11
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-02 13:07:52 +0200

    add comments and instructions how to make a test connection

--- src/main/java/org/example/HibernateUtil.java
+++ src/main/java/org/example/HibernateUtil.java
@@ -6,6 +6,28 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 
+// 1. dodaj w pliku pom.xml po dwóch enterach kod z dependencies
+// 2. stwórz w resources plik hibernate.cfg.xml i skopiuj tam kod do hibernate
+// 3. w workbenchu stwórz schema i usera oraz nadaj mu hasła i priviliges - dane te zapisz w hibernate.cfg.xml
+// pamietaj zeby w pliku cfg dodac nazwe bazy danych oraz na samym dole mapping -> klasy odpowiednia tam gdzie encje
+// 4. stwórz klasę HibernateUtil i skopiuj poniższy kod na tworzenie SessionFactory i zamykanie jej.
+
+// 5. //Metoda do Main na sprawdzenie łącznosci:
+
+//public static void main(String[] args) {
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//
+//        // Check database version (because there is nothing else in database)
+//        String sql = "select version()";
+//
+//        String result = (String) session.createNativeQuery(sql).getSingleResult();
+//        System.out.println(result);
+// session.close();
+
+//
+//        HibernateUtil.shutdown();
+//        }
+
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
     private static SessionFactory buildSessionFactory() {
